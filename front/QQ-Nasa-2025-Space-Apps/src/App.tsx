cba019f56c40af0e508d5e224de70b1420766a7b
alicezou-ca <alicezou.ca@gmail.com>
2025-10-04 11:52:11 -0700

    add tailwind + merge conflict

---
+++ front/QQ-Nasa-2025-Space-Apps/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.js
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hello, Tailwind in React!
+      </h1>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
