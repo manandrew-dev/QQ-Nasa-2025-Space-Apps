3f7b3378d76ad7f5dd903b76da5b6792f575beb4
kehan316 <kehanwang2023@gmail.com>
2025-10-04 17:53:44 -0700

    formattime

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-import express from 'express';
+import express from "express";
+import { calcRouter } from "./Calculations.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.get ('/', (req, res) => {
-    res.send('Hello World!');
+// Default route
+app.get("/", (req, res) => {
+  res.send("🌍 NASA API is running");
 });
+// Example endpoint: http://localhost:3000/api/calculate_prob?date=2025-10-04&time=17:15&tzone=-7
+app.use("/api", calcRouter);
 
+// Server start
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
