0d3fbf2baf43298b39865dd144e4498c035d484a
alicezou-ca <alicezou.ca@gmail.com>
2025-10-04 18:48:18 -0700

    initial commit

--- front/QQ-Nasa-2025-Space-Apps/src/App.tsx
+++ front/QQ-Nasa-2025-Space-Apps/src/App.tsx
@@ -1,13 +1,17 @@
 // src/App.js
+import './styles/Index.css'
+import SearchReturnPage from './components/SearchReturnPage';
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-5xl font-bold text-blue-600">
+    <div>
+        <SearchReturnPage></SearchReturnPage>
+      {/* <h1 className="text-5xl font-bold text-blue-600">
         Hello, Tailwind in React!
       </h1>
       <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
         Click Me
-      </button>
+      </button> */}
     </div>
   );
 }
