2b85efbcbcf124644a40722a5a0b1d9cebf34f9d
Keith Cheng <forgetfulparty@gmail.com>
2025-10-04 19:06:08 -0700

    Finsished the hash table setup

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-import express from "express";
-import { calcRouter } from "./Calculations.js";
+import express from 'express';
+import { buildURLIndex } from './buildIndex.js';
 
 const app = express();
-
 app.use(express.json());
 
-// Default route
-app.get("/", (req, res) => {
-  res.send("🌍 NASA API is running");
-});
-// Example endpoint: http://localhost:3000/api/calculate_prob?date=2025-10-04&time=17:15&tzone=-7
-app.use("/api", calcRouter);
+const DATA_FILE = `./data/data.txt`;
+await buildURLIndex(DATA_FILE);
 
-// Server start
+app.get ('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
